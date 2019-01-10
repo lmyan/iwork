46b706824cde2defc0ea4f81d633e76868d411c8
燕黎萌 <yanlimeng@yanlimengdeMacBook-Pro.local>
2019-01-10 14:30:42 +0800

    test

--- src/libs/test.js
+++ src/libs/test.js
@@ -2,3 +2,7 @@ let str = 'DATE="2019-01-09 13:34:36"'
 let reg = /(DATE="(.+)")?\s*(CLIENT_DEV_ID="(.+)")?\s*(CLIENT_OS="(.+)")?\s*/gim
 console.log(reg.exec(str))
 console.log(reg.test(str))
+
+let r2 = /^a/gim
+let s2 = "aaad\nabc\na44"
+console.log(s2.match(r2))
